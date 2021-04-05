cf58dff8d7d8213058989dd6a86b23231caa2fe9
Anand <anandp803@gmail.com>
2021-04-05 18:17:28 +0530

    updated score and high score
    updated score and high score

--- starter/script.js
+++ starter/script.js
@@ -1,40 +1,53 @@
 'use strict';
 
 let randNum=Math.trunc(Math.random()*20)+1;
-
+let score=20;
+let HighScore=0;
+let isMatched=false;
 //document.querySelector('.number').textContent=randNum;
 
-document.querySelector('.again').addEventListener('click',function(){
-    
-});
-console.log(randNum);
+
+    console.log(document.querySelector('.score').textContent);
 
 document.querySelector('.check').addEventListener('click',function(){
     //const x=Number(document.querySelector('.guess').value);
     
-    if(!randNum){
+    const original_num=Number(document.querySelector('.guess').value);  
+    console.log(original_num);
+
+    if(!original_num && !isMatched){
         document.querySelector('.message').textContent='😒 No number';
     }
     else
     {
-        const original_num=Number(document.querySelector('.guess').value);  
-        console.log(original_num);
-        if(original_num===randNum){
+        if(original_num===randNum && !isMatched){
 
             console.log(randNum);
+            isMatched=true;
             document.querySelector('.message').textContent='🎉 correct number';
-
             document.querySelector('body').style.backgroundColor='#60b347';
             document.querySelector('.number').style.width='30rem';
             document.querySelector('.number').textContent=original_num.toString();
+            score++;
+            document.querySelector('.score').textContent=score.toString();
+            console.log(score +">>>>>" + HighScore);
+            if(score > HighScore)
+            {
+                HighScore=score;
+                document.querySelector('.highscore').textContent=score.toString();
+            }
         }
         else if(original_num < randNum)
         {
             document.querySelector('.message').textContent='😒 Too Less';
+            score--;
+            document.querySelector('.score').textContent=score.toString();
         }
         else if(original_num > randNum)
         {
             document.querySelector('.message').textContent='😒 Too High';
+            score--;
+            document.querySelector('.score').textContent=score.toString();
         }
     }
 });
@@ -42,6 +55,7 @@ document.querySelector('.check').addEventListener('click',function(){
 //retry button
 document.querySelector('.again').addEventListener('click',function(){
     randNum=Math.trunc(Math.random()*20)+1;
+    isMatched=false;
     document.querySelector('.message').textContent='Start guessing...';
     document.querySelector('body').style.backgroundColor='#222';
     document.querySelector('.number').style.width='15rem';
